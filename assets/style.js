667479cbfd793bb25f80031fe107583ac0e18ac0
DustyMick <damicklo5@gmail.com>
2021-01-26 16:24:24 -0600

    variables added

--- assets/style.js
+++ assets/style.js
@@ -1,13 +1,36 @@
-var getCityWeather = function() {
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city-name");
+var cityTempertureEl = document.querySelector("#temperature");
+var cityHumidtyEl = document.querySelector("#humidity");
+var cityWindSpeedEl = document.querySelector("#wind-speed");
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var cityname = cityInputEl.value.trim();
+     if (cityname) {
+         getCityWeather(cityname);
+         
+     } else {
+         alert("Please enter a city name");
+     }
+};
+
+var getCityWeather = function(cityname) {
     //format the api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=32e4833dee72fbee848a843f91373083";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=32e4833dee72fbee848a843f91373083";
     
     //make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-        console.log(data)    
+        return response.json()
+    }).then(function(data) {  
+        displayCityName(data);  
+        console.log(data);    
     });
-  });
 };
 
-getCityWeather();
+var displayCityName = function(data) {
+
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
